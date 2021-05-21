0af3b4e7ddabb60f4f17936dde0d43d76e7ee1af
sounishnath003 <flock.sinasini@gmail.com>
2021-05-21 10:17:31 +0530

    patch v1.0.11 | exports Schema instances

--- templates/database/schema/index.ts
+++ templates/database/schema/index.ts
@@ -1,12 +1,13 @@
-
-
 /** Will configure the association Mappings (1:N) / (M:N) / (N:1)  */
 function buildAssociationsBetweenSchemas() {
 
 }
 
+/** Add all schema you want to export!! */
+const Schema = {};
 
 /** Export Schema from index.ts file */
 export {
+    Schema ,
     buildAssociationsBetweenSchemas
 };
